32637a5b3612d8a737e378926822ef936ee16036
jrb0001 <jrb0001@692b8c32.de>
2023-04-21 18:31:12 +0200

    Add page direction

--- src/epub.rs
+++ src/epub.rs
@@ -9,7 +9,7 @@ use crate::ReferenceType;
 use crate::{common, EpubContent};
 
 use std::io;
-use std::io::Read;
+use std::io::{ErrorKind, Read};
 use std::path::Path;
 
 use eyre::{bail, Context, Result};
@@ -27,12 +27,52 @@ pub enum EpubVersion {
     V30,
 }
 
+/// The page-progression-direction attribute of spine is a global attribute and
+/// therefore defines the pagination flow of the book as a whole.
+#[derive(Debug, Copy, Clone, Default)]
+pub enum PageDirection {
+    /// Left to right
+    #[default]
+    Ltr,
+    /// Right to left
+    Rtl,
+}
+
+impl ToString for PageDirection {
+    fn to_string(&self) -> String {
+        match &self {
+            PageDirection::Rtl => "rtl".into(),
+            PageDirection::Ltr => "ltr".into(),
+        }
+    }
+}
+
+impl Into<String> for PageDirection{
+    fn into(self) -> String {
+        self.to_string()
+    }
+}
+
+impl std::str::FromStr for PageDirection {
+    type Err = std::io::Error;
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        let s = s.to_lowercase();
+        match s.as_ref() {
+            "rtl" => Ok(PageDirection::Rtl),
+            "ltr" => Ok(PageDirection::Ltr),
+            _ => Err(std::io::Error::new(ErrorKind::Other,"Invalid PageDirection.")),
+        }
+    }
+}
+
 /// EPUB Metadata
 #[derive(Debug)]
 struct Metadata {
     pub title: String,
     pub author: Vec<String>,
     pub lang: String,
+    pub direction: PageDirection,
     pub generator: String,
     pub toc_name: String,
     pub description: Vec<String>,
@@ -50,6 +90,7 @@ impl Metadata {
             title: String::new(),
             author: vec![],
             lang: String::from("en"),
+            direction: PageDirection::default(),
             generator: String::from("Rust EPUB library"),
             toc_name: String::from("Table Of Contents"),
             description: vec![],
@@ -187,6 +228,7 @@ impl<Z: Zip> EpubBuilder<Z> {
             }
             "title" => self.metadata.title = value.into(),
             "lang" => self.metadata.lang = value.into(),
+            "direction" => self.metadata.direction = value.into().parse()?,
             "generator" => self.metadata.generator = value.into(),
             "description" => {
                 let value = value.into();
@@ -522,6 +564,7 @@ impl<Z: Zip> EpubBuilder<Z> {
                     }
                     builder
                 })
+                .insert_str("direction", self.metadata.direction.to_string())
                 .insert_str("title", html_escape::encode_text(&self.metadata.title))
                 .insert_str("generator_attr", html_escape::encode_double_quoted_attribute(&self.metadata.generator))
                 .insert_str("toc_name", html_escape::encode_text(&self.metadata.toc_name))
